6498c3f238b51961c819b6e2500f48f44923742a
Mykhailo <mykhailo.tymchyshyn@gmail.com>
2021-02-08 02:16:50 +0200

    Initial Commit

---
+++ IronFPS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IronFPS
+//
+//  Created by Mykhailo Tymchyshyn on 08.02.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
